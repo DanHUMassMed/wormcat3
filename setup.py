a734f8e7e5fd67b9268d7ea31c6e8d8034f5303f
danhumassmed <daniel.higgins@umassmed.edu>
2025-05-18 19:22:48 -0400

    Bump version to 0.0.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Setup for pypi releases of wormcat3
 from setuptools import setup, find_packages
 from pathlib import Path
 
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
